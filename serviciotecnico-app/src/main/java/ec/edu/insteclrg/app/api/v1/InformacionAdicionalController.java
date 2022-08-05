6c2dfc818273887539a88ca3aeec9cfb4e731e24
ValeriaTigre <valeria.tigre@insteclrg.edu.ec>
2022-08-05 17:29:05 -0500

    InformacionAdicionalController Update

--- serviciotecnico-app/src/main/java/ec/edu/insteclrg/app/api/v1/InformacionAdicionalController.java
+++ serviciotecnico-app/src/main/java/ec/edu/insteclrg/app/api/v1/InformacionAdicionalController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import ec.edu.insteclrg.domain.InformacionAdicional;
 import ec.edu.insteclrg.dto.InformacionAdicionalDTO;
 import ec.edu.insteclrg.service.crud.InformacionAdicionalService;
 
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping(value = "/api/v1.0/informacionadicional")
 public class InformacionAdicionalController {
