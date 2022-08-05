bc9a096eabf6987c52928ecae0bb27da9e003509
ValeriaTigre <valeria.tigre@insteclrg.edu.ec>
2022-08-05 17:26:26 -0500

    Tecnico Controller Update

--- serviciotecnico-app/src/main/java/ec/edu/insteclrg/app/api/v1/TecnicoController.java
+++ serviciotecnico-app/src/main/java/ec/edu/insteclrg/app/api/v1/TecnicoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import ec.edu.insteclrg.domain.Tecnico;
 import ec.edu.insteclrg.dto.TecnicoDTO;
 import ec.edu.insteclrg.service.crud.TecnicoService;
 
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping(value = "/api/v1.0/tecnico")
 public class TecnicoController {
